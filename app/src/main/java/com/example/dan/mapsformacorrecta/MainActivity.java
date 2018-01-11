fa2c02d1f2e79418e2db75d1396bae1b6f1e0568
Daniel Salazar <dsspombo@outlook.com>
2018-01-11 17:19:18 -0600

    Mapa y navigationDrawer agregado

--- app/src/main/java/com/example/dan/mapsformacorrecta/MainActivity.java
+++ app/src/main/java/com/example/dan/mapsformacorrecta/MainActivity.java
@@ -1,32 +1,60 @@
 package com.example.dan.mapsformacorrecta;
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
+        ,OnMapReadyCallback{
 
-    GoogleMap mimapa;
+    private GoogleMap mimapa;
+    private SupportMapFragment fragmento;
+    int status;
+    Dialog aviso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
 
         if (servicesDisponible()) {
+
+            android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            DrawerLayout drawer = findViewById(R.id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.addDrawerListener(toggle);
+            toggle.syncState();
+
+            NavigationView navigationView = findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
+
             Toast.makeText(this, "Perfecto", Toast.LENGTH_LONG).show();
-            setContentView(R.layout.activity_main);
             iniciaMapa();
         }
     }
@@ -39,11 +67,13 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         if (disponible == ConnectionResult.SUCCESS) {
             return true;
-        } else if (api.isUserResolvableError(disponible)) {
+        }
+        else if (api.isUserResolvableError(disponible)) {
 
             Dialog error = api.getErrorDialog(this, disponible, 0);
             error.show();
-        } else {
+        }
+        else {
             Toast.makeText(this, "No se puede conectar a play services", Toast.LENGTH_LONG).show();
         }
 
@@ -53,15 +83,87 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private void iniciaMapa() {
 
-        MapFragment fragmento = (MapFragment) getFragmentManager().findFragmentById(R.id.map_fragment);
+        fragmento = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa_telmex);
         fragmento.getMapAsync(this);
     }
+
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+
+        android.support.v4.app.FragmentManager sfm = getSupportFragmentManager();
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera){
+
+            if( fragmento.isHidden() ){
+                sfm.beginTransaction().show(fragmento).commit();
+            }
+
+            fragmento.getMapAsync(this);
+        }
+        else if (id == R.id.nav_gallery) {
+
+
+        }else if (id == R.id.nav_slideshow) {
+
+        }
+        else if (id == R.id.nav_manage) {
+
+        }
+        else if (id == R.id.nav_share) {
+
+        }
+        else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        Toast.makeText(this, "EN MAPrEADY", Toast.LENGTH_LONG).show();
         mimapa = googleMap;
         mimapa.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         UiSettings ajustes = mimapa.getUiSettings();
