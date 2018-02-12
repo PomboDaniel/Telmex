9eba339bc08eef4babd2e836496d149d1abdad9b
Daniel Salazar <bournultimatum@hotmail.com>
2018-02-12 10:46:10 -0600

    Design con imagen en boton para ventana principal viewpager

--- app/src/main/java/com/example/dan/mapsformacorrecta/Fragment1.java
+++ app/src/main/java/com/example/dan/mapsformacorrecta/Fragment1.java
@@ -1,34 +1,66 @@
 package com.example.dan.mapsformacorrecta;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.net.Uri;
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
+import android.widget.ImageButton;
 import java.util.ArrayList;
-
 
 public class Fragment1 extends Fragment {
 
     public static ArrayList<Regiones> lista_regiones;
     public static Adapter adapter;
+    public static ImageButton btnCos, btnVer, btnSax;
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment1, container, false);
 
-        RecyclerView recyclerView = root.findViewById(R.id.recyclerFrag1);
+        btnCos = root.findViewById(R.id.idCos);
+        btnVer = root.findViewById(R.id.idVer);
+        btnSax = root.findViewById(R.id.idSax);
+
+        lista_regiones = new ArrayList<>();
+        llenaRegiones();
+
+        btnVer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                intent.putExtra("position_card", 0);
+                startActivity(intent);
+            }
+        });
+
+        btnCos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                intent.putExtra("position_card", 2);
+                startActivity(intent);
+            }
+        });
+
+        btnSax.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                intent.putExtra("position_card", 1);
+                startActivity(intent);
+            }
+        });
+
+
+        //************EN CASO QUE SE QUIERA USAR UN RECYLCER**********
+
+        /*RecyclerView recyclerView = root.findViewById(R.id.recyclerFrag1);
 
         //recyclerView.addItemDecoration(new DividerItemDecoration(getActivity().getDrawable(R.drawable.line2), false, false));
         recyclerView.addItemDecoration(new SpaceItemDecoration(getActivity(), R.dimen.list_space, true, true));
@@ -37,11 +69,9 @@ public class Fragment1 extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setHasFixedSize(true);
 
-        lista_regiones = new ArrayList<>();
-        llenaRegiones();
         adapter = new Adapter(lista_regiones, getContext());
 
-        recyclerView.setAdapter(adapter);
+        recyclerView.setAdapter(adapter);*/
 
         /*root.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/dan/mapsformacorrecta/Fragment2.java
+++ app/src/main/java/com/example/dan/mapsformacorrecta/Fragment2.java
@@ -1,19 +1,15 @@
 package com.example.dan.mapsformacorrecta;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import static com.example.dan.mapsformacorrecta.Fragment1.btnCos;
+import static com.example.dan.mapsformacorrecta.Fragment1.btnSax;
+import static com.example.dan.mapsformacorrecta.Fragment1.btnVer;
 
-import java.util.ArrayList;
-
-import static com.example.dan.mapsformacorrecta.Fragment1.adapter;
-import static com.example.dan.mapsformacorrecta.Fragment1.lista_regiones;
-import static com.example.dan.mapsformacorrecta.Fragment1.llenaRegiones;
 
 
 public class Fragment2 extends Fragment {
@@ -21,27 +17,41 @@ public class Fragment2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment2, container, false);
+        View root = inflater.inflate(R.layout.fragment1, container, false);
 
-        RecyclerView recyclerView = root.findViewById(R.id.recyclerFrag2);
+        btnCos = root.findViewById(R.id.idCos);
+        btnVer = root.findViewById(R.id.idVer);
+        btnSax = root.findViewById(R.id.idSax);
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setHasFixedSize(true);
-
-        lista_regiones = new ArrayList<>();
-        llenaRegiones();
-        adapter = new Adapter(lista_regiones, getContext());
-
-        recyclerView.setAdapter(adapter);
-
-
-        /*root.setOnClickListener(new View.OnClickListener() {
+        btnVer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Toast.makeText(getActivity(), "click en Radio-bases! ", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                intent.putExtra("position_card", 0);
+                startActivity(intent);
             }
-        });*/
+        });
+
+        btnCos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                intent.putExtra("position_card", 2);
+                startActivity(intent);
+            }
+        });
+
+        btnSax.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                intent.putExtra("position_card", 1);
+                startActivity(intent);
+            }
+        });
 
         return root;
     }
